ec7b8f468b562774f583f13707665dca3b11d199
Chukwukagideon <Chukwukagideon5@gmail.com>
2025-03-15 23:07:39 +0100

    updated plot style and used color map. added code to save plots automatically

--- GENERATING DATA/scatter_squares.py
+++ GENERATING DATA/scatter_squares.py
@@ -7,9 +7,9 @@ x_values = range(1, 1001)
 y_values = [x**2 for x in x_values]
 
 
-plt.style.use("classic")
+plt.style.use("seaborn-v0_8-dark")
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, c="red", s=10)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 # Set Chart title and label Axes
 ax.set_title("Square Numbers", fontsize=24)
@@ -23,3 +23,4 @@ ax.tick_params(axis="both", which="major", labelsize=14)
 ax.axis([0, 1100, 0, 1100000])
 
 plt.show()
+# plt.savefig("squares_plot.png", bbox_inches = "tight")
