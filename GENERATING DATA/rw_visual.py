c8253bbae7028c4d1cd3ee6a2fd6d0a6618451ee
Chukwukagideon <Chukwukagideon5@gmail.com>
2025-03-21 14:38:34 +0100

    add rw_visual.py to visualize random walks using matplotlib

---
+++ GENERATING DATA/rw_visual.py
@@ -0,0 +1,13 @@
+from matplotlib import pyplot as plt
+
+from random_walk import RandomWalk
+
+# Make a random walk.
+rw = RandomWalk()
+rw.fill_walk()
+
+# Plot the points in the walk.
+plt.style.use("seaborn-v0_8-dark")
+fig, ax = plt.subplots()
+ax.scatter(rw.x_values, rw.y_values, s=15)
+plt.show()
