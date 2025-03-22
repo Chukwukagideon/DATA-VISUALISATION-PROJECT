418c93050db803f045dc04f1e61fc0ef68d140b2
Chukwukagideon <Chukwukagideon5@gmail.com>
2025-03-22 16:27:03 +0100

    enhanced die_visual.py to visualize the results of rolling a D6 1000 times using a histogram

--- GENERATING DATA/plotly/die_visual.py
+++ GENERATING DATA/plotly/die_visual.py
@@ -1,3 +1,7 @@
+from plotly.graph_objs import Bar, Layout
+
+from plotly import offline
+
 from die import Die
 
 # Create a D6
@@ -10,10 +14,17 @@ for roll_num in range(1000):
     result = die.roll()
     results.append(result)
 
-# Analysing the result
+# Analyzing the result
 frequencies = []
 for value in range(1, die.num_sides + 1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
-print(frequencies)
+# Visualize the results.
+x_values = list(range(1, die.num_sides + 1))
+data = [Bar(x=x_values, y=frequencies)]
+
+x_axis_config = {"title": "Result"}
+y_axis_config = {"title": "Frequency of result"}
+my_layout = Layout(title="The result of rolling one D6 1000 times", xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({"data": data, "layout": my_layout}, filename="D6.html")
