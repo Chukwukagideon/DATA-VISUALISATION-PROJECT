9df8326e4b044085e04d87d135e5e80a52c427de
Chukwukagideon <Chukwukagideon5@gmail.com>
2025-03-24 23:19:28 +0100

    updated sitka_highs.py: updated code to plot temperature values for a whole year

--- DOWNLOADING DATA/sitka_highs.py
+++ DOWNLOADING DATA/sitka_highs.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 
-filename = "data/sitka_weather_07-2018_simple.csv"
+filename = "data/sitka_weather_2018_simple.csv"
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -22,7 +22,7 @@ fig, ax = plt.subplots()
 ax.plot(dates, highs, c="red")
 
 # Format plot
-plt.title("Daily High Temperatures, July 2018", fontsize=24)
+plt.title("Daily High Temperatures - 2018", fontsize=24)
 plt.xlabel("", fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
