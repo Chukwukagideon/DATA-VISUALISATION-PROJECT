c5108d94c874b9d1def648202ca749ca6cfc64ad
Chukwukagideon <Chukwukagideon5@gmail.com>
2025-03-25 08:41:56 +0100

    updated sitka_highs_lows.py: added low temperature data and updated plot to display both high and low temperatures for 2018

--- DOWNLOADING DATA/sitka_highs_lows.py
+++ DOWNLOADING DATA/sitka_highs_lows.py
@@ -8,21 +8,26 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         date = datetime.strptime(row[2], "%Y-%m-%d")
         dates.append(date)
+
         high = int(row[5])
         highs.append(high)
 
+        low = int(row[-1])
+        lows.append(low)
 
-# Plot the high temperatures
+
+# Plot high and low temperatures
 plt.style.use("seaborn-v0_8-dark")
 fig, ax = plt.subplots()
 ax.plot(dates, highs, c="red")
+ax.plot(dates, lows, c="blue")
 
 # Format plot
-plt.title("Daily High Temperatures - 2018", fontsize=24)
+plt.title("Daily High and Low Temperatures - 2018", fontsize=24)
 plt.xlabel("", fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
