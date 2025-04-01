e8645de25e0f315d112a26a9edd99013f291447d
Chukwukagideon <Chukwukagideon5@gmail.com>
2025-04-01 16:40:18 +0100

    Updated death_valley_highs_lows.py to handle error that may arise from missing data

--- DOWNLOADING DATA/death_valley_highs_lows.py
+++ DOWNLOADING DATA/death_valley_highs_lows.py
@@ -14,13 +14,15 @@ with open(filename) as f:
     dates, highs, lows = [], [], []
     for row in reader:
         date = datetime.strptime(row[2], "%Y-%m-%d")
-        dates.append(date)
-
-        high = int(row[4])
-        highs.append(high)
-
-        low = int(row[5])
-        lows.append(low)
+        try:
+            high = int(row[4])
+            low = int(row[5])
+        except ValueError:
+            print(f"Missing data values for {date}")
+        else:
+            dates.append(date)
+            highs.append(high)
+            lows.append(low)
 
 
 # Plot high and low temperatures
@@ -31,7 +33,7 @@ ax.plot(dates, lows, c="blue", alpha=0.5)
 plt.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
 
 # Format plot
-plt.title("Daily High and Low Temperatures - 2018", fontsize=24)
+plt.title("Daily High and Low Temperatures - 2018\n Death Valley, CA", fontsize=20)
 plt.xlabel("", fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
