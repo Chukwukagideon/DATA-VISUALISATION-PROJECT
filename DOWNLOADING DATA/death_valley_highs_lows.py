82754389f2a2ebcd3a0194e5334140d3584a2c9b
Chukwukagideon <Chukwukagideon5@gmail.com>
2025-04-01 16:12:54 +0100

    added death_valley_highs_lows.py which plots the temperature high and low for each day in death valley

---
+++ DOWNLOADING DATA/death_valley_highs_lows.py
@@ -0,0 +1,40 @@
+import csv
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+
+filename = "data/death_valley_2018_simple.csv"
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    # for index, column_header in enumerate(header_row):
+    #     print(index, column_header)
+
+    dates, highs, lows = [], [], []
+    for row in reader:
+        date = datetime.strptime(row[2], "%Y-%m-%d")
+        dates.append(date)
+
+        high = int(row[4])
+        highs.append(high)
+
+        low = int(row[5])
+        lows.append(low)
+
+
+# Plot high and low temperatures
+plt.style.use("seaborn-v0_8-dark")
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c="red", alpha=0.5)
+ax.plot(dates, lows, c="blue", alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
+
+# Format plot
+plt.title("Daily High and Low Temperatures - 2018", fontsize=24)
+plt.xlabel("", fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis="both", which="major", labelsize=16)
+
+plt.show()
