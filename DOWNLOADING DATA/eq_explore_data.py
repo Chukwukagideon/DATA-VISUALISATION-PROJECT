83fd32d229bfe627cf614d62b1db53c45b93cdaf
Chukwukagideon <Chukwukagideon5@gmail.com>
2025-04-13 18:41:32 +0100

    making a list of all the earthquakes in eq_data_1_day_m1.json

--- DOWNLOADING DATA/eq_explore_data.py
+++ DOWNLOADING DATA/eq_explore_data.py
@@ -9,3 +9,6 @@ with open(filename) as f:
 readable_file = "data/earthquake/readable_eq_data.json"
 with open(readable_file, "w") as f:
     json.dump(all_eq_data, f, indent=4)
+
+all_eq_dicts = all_eq_data["features"]
+print(len(all_eq_dicts))
