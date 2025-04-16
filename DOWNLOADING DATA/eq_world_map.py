5070c7200ffe191a2eb598a8dd4b53656a057f9c
Chukwukagideon <Chukwukagideon5@gmail.com>
2025-04-16 15:34:55 +0100

    refactor data structure for earthquake plotting in eq_world_map.py

--- DOWNLOADING DATA/eq_world_map.py
+++ DOWNLOADING DATA/eq_world_map.py
@@ -4,7 +4,15 @@ from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
 # Map the Earthquakes
-data = [Scattergeo(lon=eq.lons, lat=eq.lats)]
+# data = [Scattergeo(lon=lons, lat=lats)]
+
+# OR
+
+data = [{
+    "type": "scattergeo",
+    "lon": eq.lons,
+    "lat": eq.lats
+}]
 my_layout = Layout(title="Global Earthquakes")
 fig = {"data":data, "layout":my_layout}
 offline.plot(fig, filename="global_earthquakes.html")
